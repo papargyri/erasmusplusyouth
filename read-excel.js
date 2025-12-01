ce6a6e88bcb5a47d7500398618932ce5f96883f7
Lamprini Papargyri <papargyri1@gmail.com>
2025-11-30 21:07:47 -0800

    linked.xlxs

---
+++ read-excel.js
@@ -0,0 +1,14 @@
+const XLSX = require('xlsx');
+
+const workbook = XLSX.readFile('European Collaborative Alliance - Registration Form (Responses) (2).xlsx');
+const sheetName = workbook.SheetNames[0];
+const sheet = workbook.Sheets[sheetName];
+const data = XLSX.utils.sheet_to_json(sheet, { header: 1 });
+
+console.log('Column headers:');
+console.log(data[0]);
+console.log('\nFirst 5 rows of data:');
+for (let i = 1; i <= 5 && i < data.length; i++) {
+    console.log(`Row ${i}:`, data[i]);
+}
+console.log(`\nTotal rows: ${data.length - 1}`);
